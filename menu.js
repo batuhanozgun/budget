632099e0fc9018deed7568cc275f6265e4d675f0
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-19 21:18:48 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-const auth = getAuth();
+// Firebase konfigürasyonu
+const firebaseConfig = {
+    apiKey: "AIzaSyDidWK1ghqKTzokhT-YoqGb7Tz9w5AFjhM",
+    authDomain: "batusbudget.firebaseapp.com",
+    projectId: "batusbudget",
+    storageBucket: "batusbudget.appspot.com",
+    messagingSenderId: "1084998760222",
+    appId: "1:1084998760222:web:d28492021d0ccefaf2bb0f"
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export function loadMenu() {
     fetch('menu.html')
