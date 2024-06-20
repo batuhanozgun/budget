db4224a1cacd2a0be94dd345d9d5b133c6cd1d1a
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 01:09:52 +0300

    Update login.js

--- login.js
+++ login.js
@@ -7,12 +7,13 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const messageDiv = document.getElementById('message');
 
     try {
         await signInWithEmailAndPassword(auth, email, password);
         window.location.href = 'landing.html';
     } catch (error) {
         console.error('Giriş hatası: ', error);
-        alert('Giriş hatası: ' + error.message);
+        messageDiv.textContent = 'Giriş hatası: ' + error.message;
     }
 });
