068158083917fcbede22d053a0603812a48f5abd
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 01:40:03 +0300

    Create landing.js

---
+++ landing.js
@@ -0,0 +1,16 @@
+// landing.js
+import { auth } from './firebaseConfig.js';
+import { signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+document.getElementById('logoutButton').addEventListener('click', async () => {
+    const messageDiv = document.getElementById('message');
+    
+    try {
+        await signOut(auth);
+        window.location.href = 'login.html';
+    } catch (error) {
+        console.error('Çıkış hatası: ', error);
+        messageDiv.textContent = 'Çıkış hatası: ' + error.message;
+        messageDiv.style.display = 'block';
+    }
+});
