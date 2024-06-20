e5818393626cf4941084e208dbd1c8b2ecdd025f
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-20 18:36:55 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDidWK1ghqKTzokhT-YoqGb7Tz9w5AFjhM",
+    authDomain: "batusbudget.firebaseapp.com",
+    projectId: "batusbudget",
+    storageBucket: "batusbudget.appspot.com",
+    messagingSenderId: "1084998760222",
+    appId: "1:1084998760222:web:d28492021d0ccefaf2bb0f"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
