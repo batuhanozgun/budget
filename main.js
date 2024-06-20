aee5832d866f1a63cbb5400226f902a533762fcf
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-20 23:16:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import { loadMenu, logout } from './menu.js';
 import { auth } from './firebaseConfig.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadMenu();
+    // Sadece index.html dışında menüyü yükle
+    if (document.body.getAttribute('data-page') !== 'index') {
+        loadMenu();
 
-    document.addEventListener('click', (event) => {
-        if (event.target && event.target.id === 'logoutButton') {
-            logout(auth);
-        }
-    });
+        document.addEventListener('click', (event) => {
+            if (event.target && event.target.id === 'logoutButton') {
+                logout(auth);
+            }
+        });
+    }
 });
