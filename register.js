ac83d7bad4a437fc2ce85468bd4ccdb1ccfbf223
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-20 20:25:29 +0300

    Update register.js

--- register.js
+++ register.js
@@ -1,22 +1,15 @@
 // register.js
 import { auth } from './firebaseConfig.js';
-import { createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 document.getElementById('registerForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-
-        await updateProfile(user, {
-            displayName: username
-        });
-
+        await createUserWithEmailAndPassword(auth, email, password);
         alert('Kayıt başarılı!');
         window.location.href = 'landing.html';
     } catch (error) {
