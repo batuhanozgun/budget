44b394c1996a49827040b5fe44abb966a4f6a847
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 01:03:21 +0300

    Create login.cs

---
+++ login.cs
@@ -0,0 +1,19 @@
+// login.js
+import { auth } from './firebaseConfig.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        alert('Giriş başarılı!');
+        window.location.href = 'landing.html';
+    } catch (error) {
+        console.error('Giriş hatası: ', error);
+        alert('Giriş hatası: ' + error.message);
+    }
+});
