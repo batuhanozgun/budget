013b5c0dae7b23759a51855fcaecf13dd40792a2
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 08:32:25 +0300

    Update kredi.js

--- kredi.js
+++ kredi.js
@@ -2,23 +2,29 @@ export function getKrediFields() {
     return `
         <label for="loanAmount">Kredi Tutarı:</label>
         <input type="number" step="0.01" id="loanAmount" name="loanAmount" required>
-        <label for="disbursedAmount">Hesaba Yatan Tutar:</label>
-        <input type="number" step="0.01" id="disbursedAmount" name="disbursedAmount" required>
-        <label for="expenses">Masraflar:</label>
-        <input type="number" step="0.01" id="expenses" name="expenses" required>
-        <label for="insurance">Sigorta:</label>
-        <input type="number" step="0.01" id="insurance" name="insurance" required>
-        <label for="installments">Vade:</label>
-        <input type="number" id="installments" name="installments" required>
+        <label for="fundRate">Fon Oranı (%):</label>
+        <input type="number" step="0.01" id="fundRate" name="fundRate" required>
+        <label for="taxRate">Vergi Oranı (%):</label>
+        <input type="number" step="0.01" id="taxRate" name="taxRate" required>
+        <label for="remainingTerm">Kalan Vade:</label>
+        <input type="number" id="remainingTerm" name="remainingTerm" required>
+        <label for="installmentAmount">Taksit Tutarı:</label>
+        <input type="number" step="0.01" id="installmentAmount" name="installmentAmount" required>
+        <label for="totalTerm">Toplam Vade:</label>
+        <input type="number" id="totalTerm" name="totalTerm" required>
+        <label for="loanInterestRate">Faiz Oranı (%):</label>
+        <input type="number" step="0.01" id="loanInterestRate" name="loanInterestRate" required>
     `;
 }
 
 export function getKrediValues() {
     return {
         loanAmount: parseFloat(document.getElementById('loanAmount').value).toFixed(2),
-        disbursedAmount: parseFloat(document.getElementById('disbursedAmount').value).toFixed(2),
-        expenses: parseFloat(document.getElementById('expenses').value).toFixed(2),
-        insurance: parseFloat(document.getElementById('insurance').value).toFixed(2),
-        installments: parseInt(document.getElementById('installments').value)
+        fundRate: parseFloat(document.getElementById('fundRate').value).toFixed(2),
+        taxRate: parseFloat(document.getElementById('taxRate').value).toFixed(2),
+        remainingTerm: parseInt(document.getElementById('remainingTerm').value),
+        installmentAmount: parseFloat(document.getElementById('installmentAmount').value).toFixed(2),
+        totalTerm: parseInt(document.getElementById('totalTerm').value),
+        loanInterestRate: parseFloat(document.getElementById('loanInterestRate').value).toFixed(2)
     };
 }
