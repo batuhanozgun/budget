549dfbeecbac7b1ab74635ed1c9270a38c3f3a28
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 08:45:43 +0300

    Update krediKarti.js

--- krediKarti.js
+++ krediKarti.js
@@ -2,30 +2,29 @@ export function getKrediKartiFields() {
     return `
         <label for="cardLimit">Kart Limiti:</label>
         <input type="number" step="0.01" id="cardLimit" name="cardLimit" required>
-        <label for="billingCycle">Hesap Kesim Dönemi:</label>
-        <select id="billingCycle" name="billingCycle" required>
-            <option value="specificDay">Her ayın belirli bir günü</option>
-            <option value="workDay">Her ayın belirli bir iş günü</option>
-        </select>
-        <div id="billingCycleDetails"></div>
+        <label for="availableLimit">Kullanılabilir Limit (Kalan Limit):</label>
+        <input type="number" step="0.01" id="availableLimit" name="availableLimit" required>
+        <label for="currentSpending">Dönem İçi Harcama:</label>
+        <input type="number" step="0.01" id="currentSpending" name="currentSpending" required>
+        <label for="pendingAmountAtOpening">Hesap Açılışındaki Bekleyen Tutar:</label>
+        <input type="number" step="0.01" id="pendingAmountAtOpening" name="pendingAmountAtOpening" required>
+        <label for="previousStatementBalance">Bir Önceki Ekstreden Kalan Tutar:</label>
+        <input type="number" step="0.01" id="previousStatementBalance" name="previousStatementBalance" required>
+        <label for="statementDate">Ekstre Kesim Tarihi:</label>
+        <input type="date" id="statementDate" name="statementDate" required>
+        <label for="paymentDueDate">Son Ödeme Tarihi:</label>
+        <input type="date" id="paymentDueDate" name="paymentDueDate" required>
     `;
 }
 
 export function getKrediKartiValues() {
     return {
         cardLimit: parseFloat(document.getElementById('cardLimit').value).toFixed(2),
-        billingCycle: document.getElementById('billingCycle').value,
-        billingCycleDetails: getBillingCycleDetails()
+        availableLimit: parseFloat(document.getElementById('availableLimit').value).toFixed(2),
+        currentSpending: parseFloat(document.getElementById('currentSpending').value).toFixed(2),
+        pendingAmountAtOpening: parseFloat(document.getElementById('pendingAmountAtOpening').value).toFixed(2),
+        previousStatementBalance: parseFloat(document.getElementById('previousStatementBalance').value).toFixed(2),
+        statementDate: document.getElementById('statementDate').value,
+        paymentDueDate: document.getElementById('paymentDueDate').value
     };
-}
-
-function getBillingCycleDetails() {
-    const billingCycle = document.getElementById('billingCycle').value;
-    let details = '';
-    if (billingCycle === 'specificDay') {
-        details = document.getElementById('billingDay').value;
-    } else if (billingCycle === 'workDay') {
-        details = document.getElementById('billingWorkDay').value;
-    }
-    return details;
 }
