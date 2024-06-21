26c2336a2ebb4f121e9eb4413675e211b8ebc00a
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 03:13:13 +0300

    Create nakit.js

---
+++ nakit.js
@@ -0,0 +1,11 @@
+export function getNakitFields() {
+    return `
+        <label for="initialBalance">Başlangıç Bakiyesi:</label>
+        <input type="number" id="initialBalance" name="initialBalance" required>`;
+}
+
+export function getNakitValues() {
+    return {
+        initialBalance: document.getElementById('initialBalance').value
+    };
+}
