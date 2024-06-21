91f0c2fd4c1ed5804d4fbeab4c89e1fd943a6ebe
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 03:32:51 +0300

    Update krediKarti.js

--- krediKarti.js
+++ krediKarti.js
@@ -18,19 +18,3 @@ export function getKrediKartiValues() {
         billingWorkDay: document.getElementById('billingWorkDay') ? document.getElementById('billingWorkDay').value : null
     };
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('billingCycle').addEventListener('change', updateBillingCycleDetails);
-});
-
-function updateBillingCycleDetails() {
-    const billingCycle = document.getElementById('billingCycle').value;
-    const billingCycleDetails = document.getElementById('billingCycleDetails');
-    billingCycleDetails.innerHTML = '';
-
-    if (billingCycle === 'specificDay') {
-        billingCycleDetails.innerHTML += '<label for="billingDay">Gün:</label><input type="number" id="billingDay" name="billingDay" min="1" max="31" required>';
-    } else if (billingCycle === 'workDay') {
-        billingCycleDetails.innerHTML += '<label for="billingWorkDay">İş Günü:</label><select id="billingWorkDay" name="billingWorkDay" required><option value="first">Ayın ilk iş günü</option><option value="last">Ayın son iş günü</option></select>';
-    }
-}
