c1c55b0a5c3a328ad03a510ffb523f8b4bbeb422
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 07:36:10 +0300

    Update banka.js

--- banka.js
+++ banka.js
@@ -4,12 +4,15 @@ export function getBankaFields() {
         <input type="number" id="initialBalance" name="initialBalance" required>
         <label for="overdraftLimit">KMH Limiti:</label>
         <input type="number" id="overdraftLimit" name="overdraftLimit" required>
+        <label for="overdraftInterestRate">KMH Faizi (%):</label>
+        <input type="number" step="0.01" id="overdraftInterestRate" name="overdraftInterestRate" required>
     `;
 }
 
 export function getBankaValues() {
     return {
         initialBalance: document.getElementById('initialBalance').value,
-        overdraftLimit: document.getElementById('overdraftLimit').value
+        overdraftLimit: document.getElementById('overdraftLimit').value,
+        overdraftInterestRate: document.getElementById('overdraftInterestRate').value
     };
 }
