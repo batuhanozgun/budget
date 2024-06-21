569a1decf387671df33d265f466ae2e6222ad6e7
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 08:53:14 +0300

    Update birikim.js

--- birikim.js
+++ birikim.js
@@ -1,31 +1,15 @@
 export function getBirikimFields() {
     return `
-        <label for="targetAmount">Hedef Tutar:</label>
+        <label for="targetAmount">Düzenli Ödenen Tutar:</label>
         <input type="number" step="0.01" id="targetAmount" name="targetAmount" required>
-        <label for="paymentFrequency">Ödeme Sıklığı:</label>
-        <select id="paymentFrequency" name="paymentFrequency" required>
-            <option value="weekly">Her hafta</option>
-            <option value="monthly">Her ay</option>
-        </select>
-        <div id="paymentFrequencyDetails"></div>
+        <label for="targetDate">Ödeme Yapılacak Hedef Tarih:</label>
+        <input type="date" id="targetDate" name="targetDate" required>
     `;
 }
 
 export function getBirikimValues() {
     return {
         targetAmount: parseFloat(document.getElementById('targetAmount').value).toFixed(2),
-        paymentFrequency: document.getElementById('paymentFrequency').value,
-        paymentFrequencyDetails: getPaymentFrequencyDetails()
+        targetDate: document.getElementById('targetDate').value
     };
-}
-
-function getPaymentFrequencyDetails() {
-    const paymentFrequency = document.getElementById('paymentFrequency').value;
-    let details = '';
-    if (paymentFrequency === 'weekly') {
-        details = document.getElementById('paymentDay').value;
-    } else if (paymentFrequency === 'monthly') {
-        details = document.getElementById('paymentDate').value;
-    }
-    return details;
 }
