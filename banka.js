ab0bdaa7ce820f567276f3d1d0f594a7c2ad4e48
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 07:42:52 +0300

    Update banka.js

--- banka.js
+++ banka.js
@@ -1,9 +1,9 @@
 export function getBankaFields() {
     return `
         <label for="initialBalance">Başlangıç Bakiyesi:</label>
-        <input type="number" id="initialBalance" name="initialBalance" step="0.001" required>
+        <input type="number" id="initialBalance" name="initialBalance" step="0.01" required>
         <label for="overdraftLimit">KMH Limiti:</label>
-        <input type="number" id="overdraftLimit" name="overdraftLimit" step="0.001" required>
+        <input type="number" id="overdraftLimit" name="overdraftLimit" step="0.01" required>
         <label for="overdraftInterestRate">KMH Faizi (%):</label>
         <input type="number" step="0.01" id="overdraftInterestRate" name="overdraftInterestRate" required>
     `;
@@ -11,8 +11,8 @@ export function getBankaFields() {
 
 export function getBankaValues() {
     return {
-        initialBalance: parseFloat(document.getElementById('initialBalance').value).toFixed(3),
-        overdraftLimit: parseFloat(document.getElementById('overdraftLimit').value).toFixed(3),
+        initialBalance: parseFloat(document.getElementById('initialBalance').value).toFixed(2),
+        overdraftLimit: parseFloat(document.getElementById('overdraftLimit').value).toFixed(2),
         overdraftInterestRate: parseFloat(document.getElementById('overdraftInterestRate').value).toFixed(2)
     };
 }
