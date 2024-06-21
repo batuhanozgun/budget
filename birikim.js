458b42abd05c0582870883bcee5861ec4a322442
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-21 03:33:07 +0300

    Update birikim.js

--- birikim.js
+++ birikim.js
@@ -18,19 +18,3 @@ export function getBirikimValues() {
         paymentDate: document.getElementById('paymentDate') ? document.getElementById('paymentDate').value : null
     };
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('paymentFrequency').addEventListener('change', updatePaymentFrequencyDetails);
-});
-
-function updatePaymentFrequencyDetails() {
-    const paymentFrequency = document.getElementById('paymentFrequency').value;
-    const paymentFrequencyDetails = document.getElementById('paymentFrequencyDetails');
-    paymentFrequencyDetails.innerHTML = '';
-
-    if (paymentFrequency === 'weekly') {
-        paymentFrequencyDetails.innerHTML += '<label for="paymentDay">Gün:</label><select id="paymentDay" name="paymentDay" required><option value="monday">Pazartesi</option><option value="tuesday">Salı</option><option value="wednesday">Çarşamba</option><option value="thursday">Perşembe</option><option value="friday">Cuma</option><option value="saturday">Cumartesi</option><option value="sunday">Pazar</option></select>';
-    } else if (paymentFrequency === 'monthly') {
-        paymentFrequencyDetails.innerHTML += '<label for="paymentDate">Gün:</label><input type="number" id="paymentDate" name="paymentDate" min="1" max="31" required>';
-    }
-}
