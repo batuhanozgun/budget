3728e6c8237cdc47c832d764aeb8a80ce417c8df
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-28 22:38:57 +0300

    Update register.js

--- register.js
+++ register.js
@@ -1,16 +1,29 @@
-// register.js
-import { auth } from './firebaseConfig.js';
+import { auth, db } from './firebaseConfig.js';
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 document.getElementById('registerForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
     const messageDiv = document.getElementById('message');
 
     try {
-        await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
+        // Kullanıcı bilgilerini Firestore'a kaydet
+        await setDoc(doc(db, 'users', user.uid), {
+            uid: user.uid,
+            email: user.email,
+            firstName: firstName,
+            lastName: lastName,
+            admin: false // Varsayılan olarak admin olmayan kullanıcı
+        });
+
         window.location.href = 'verify_email.html';
     } catch (error) {
         console.error('Kayıt hatası: ', error);
