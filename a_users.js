bf2e2664e487cee2b99b2fbc7c55db310c36db60
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-29 02:46:10 +0300

    Update a_users.js

--- a_users.js
+++ a_users.js
@@ -1,29 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-// Firebase yapılandırmanızı buraya ekleyin
-const firebaseConfig = {
-    apiKey: "AIzaSyDidWK1ghqKTzokhT-YoqGb7Tz9w5AFjhM",
-    authDomain: "batusbudget.firebaseapp.com",
-    projectId: "batusbudget",
-    storageBucket: "batusbudget.appspot.com",
-    messagingSenderId: "1084998760222",
-    appId: "1:1084998760222:web:d28492021d0ccefaf2bb0f"
-};
+const db = getFirestore();
+const auth = getAuth();
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        loadUsers();
+    } else {
+        window.location.href = 'login.html';
+    }
+});
 
-export async function loadUsers() {
+async function loadUsers() {
     const userList = document.getElementById('userList');
     userList.innerHTML = '';
 
     const querySnapshot = await getDocs(collection(db, 'users'));
 
     querySnapshot.forEach((doc) => {
-        const li = document.createElement('li');
         const userData = doc.data();
         const lastLogin = userData.lastLogin ? new Date(userData.lastLogin.seconds * 1000).toLocaleString() : 'Bilinmiyor';
+        const li = document.createElement('li');
         li.textContent = `${userData.email} (Son giriş: ${lastLogin})`;
         userList.appendChild(li);
     });
