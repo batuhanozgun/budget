84ef91568aada09044763818b094f2ee48bf3227
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-28 23:10:18 +0300

    Update a_users.js

--- a_users.js
+++ a_users.js
@@ -1,6 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 
-const db = getFirestore();
+// Firebase yapılandırmanızı buraya ekleyin
+const firebaseConfig = {
+    apiKey: "AIzaSyDidWK1ghqKTzokhT-YoqGb7Tz9w5AFjhM",
+    authDomain: "batusbudget.firebaseapp.com",
+    projectId: "batusbudget",
+    storageBucket: "batusbudget.appspot.com",
+    messagingSenderId: "1084998760222",
+    appId: "1:1084998760222:web:d28492021d0ccefaf2bb0f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export async function loadUsers() {
     const userList = document.getElementById('userList');
