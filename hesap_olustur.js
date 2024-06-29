d24caa041e8c71082f05426236f9ee5de244650b
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-29 18:08:47 +0300

    Update hesap_olustur.js

--- hesap_olustur.js
+++ hesap_olustur.js
@@ -1,8 +1,8 @@
 import { auth, db } from './firebaseConfig.js';
 import { checkAuth } from './auth.js';
 import { loadAccounts, loadAccountDetails, displayAccountDetails } from './HO_account.js';
-import { handleFormSubmit, handleDeleteAccount, resetForm } from './HO_formHandlers.js';
-import { updateDynamicFields, showMessage, addInstallment } from './HO_ui.js';
+import { handleFormSubmit, handleDeleteAccount } from './HO_formHandlers.js';
+import { updateDynamicFields, showMessage, addInstallment, resetForm } from './HO_ui.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const user = await checkAuth();
