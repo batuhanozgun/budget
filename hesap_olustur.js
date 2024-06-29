c2cd977b594c200036ed63c41079ecade5d281ef
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-29 14:08:31 +0300

    Update hesap_olustur.js

--- hesap_olustur.js
+++ hesap_olustur.js
@@ -1,12 +1,14 @@
 import { auth, db } from './firebaseConfig.js';
+import { collection, addDoc, getDocs, query, where, doc, getDoc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { checkAuth } from './auth.js';
 import { getNakitFields, getNakitValues, getNakitLabels } from './nakit.js';
 import { getBankaFields, getBankaValues, getBankaLabels } from './banka.js';
 import { getKrediFields, getKrediValues, getKrediLabels } from './kredi.js';
-import { getKrediKartiFields, getKrediKartiValues, getKrediKartiLabels, addInstallment } from './krediKarti.js';
+import { getKrediKartiFields, getKrediKartiValues, getKrediKartiLabels, addInstallment, getInstallmentsData } from './krediKarti.js';
 import { getBirikimFields, getBirikimValues, getBirikimLabels } from './birikim.js';
-import { handleFormSubmit, handleDeleteAccount } from './HO_formHandlers.js';
-import { loadAccounts, updateDynamicFields, resetForm } from './HO_ui.js';
+import { loadAccounts, displayAccountDetails } from './HO_account.js';
+import { handleFormSubmit, handleDeleteAccount, handleEditAccount, resetForm } from './HO_formHandlers.js';
+import { updateDynamicFields } from './HO_ui.js'; // import the function from HO_ui.js
 
 document.addEventListener('DOMContentLoaded', async () => {
     const user = await checkAuth();
@@ -18,5 +20,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.getElementById('accountForm').addEventListener('submit', handleFormSubmit);
     document.getElementById('addInstallmentButton').addEventListener('click', addInstallment);
     document.getElementById('deleteAccountButton').addEventListener('click', handleDeleteAccount);
+    document.getElementById('editAccountButton').addEventListener('click', handleEditAccount);
     document.getElementById('cancelEditButton').addEventListener('click', resetForm);
 });
