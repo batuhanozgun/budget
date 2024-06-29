9c595a448bbbd230a14225b1745df66b605a9825
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-06-29 03:50:27 +0300

    Update birikim.js

--- birikim.js
+++ birikim.js
@@ -2,14 +2,13 @@ export function getBirikimFields() {
     return `
         <label for="targetAmount">Düzenli Ödenen Tutar:</label>
         <input type="number" step="0.01" id="targetAmount" name="targetAmount" required>
-        <label for="targetDate">Ödeme Yapılacak Hedef Tarih:</label>
+        <label for="targetDate">En Yakın Ödeme Yapılacak Hedef Tarih:</label>
         <input type="date" id="targetDate" name="targetDate" required>
     `;
 }
 
 export function getBirikimValues() {
-    return {
-        targetAmount: parseFloat(document.getElementById('targetAmount').value).toFixed(2),
-        targetDate: document.getElementById('targetDate').value
-    };
+    const targetAmount = parseFloat(document.getElementById('targetAmount').value).toFixed(2);
+    const targetDate = document.getElementById('targetDate').value;
+    return { targetAmount, targetDate };
 }
