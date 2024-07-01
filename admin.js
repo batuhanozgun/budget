88e989f3b5257499f8cb5b80dc610fb39529f782
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-07-01 23:55:33 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,16 +10,24 @@ onAuthStateChanged(auth, (user) => {
         loadUsers();
         loadKayitTipleri();
         loadKayitYonleri();
-        document.getElementById('kayitTipiForm').addEventListener('submit', (e) => {
-            e.preventDefault();
-            console.log("Kayıt Tipi Formu Gönderildi");
-            addKayitTipi();
-        });
-        document.getElementById('kayitYonuForm').addEventListener('submit', (e) => {
-            e.preventDefault();
-            console.log("Kayıt Yönü Formu Gönderildi");
-            addKayitYonu();
-        });
+        const kayitTipiForm = document.getElementById('kayitTipiForm');
+        const kayitYonuForm = document.getElementById('kayitYonuForm');
+
+        if (kayitTipiForm && kayitYonuForm) {
+            console.log("Forms found");
+            kayitTipiForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+                console.log("Kayıt Tipi Formu Gönderildi");
+                addKayitTipi();
+            });
+            kayitYonuForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+                console.log("Kayıt Yönü Formu Gönderildi");
+                addKayitYonu();
+            });
+        } else {
+            console.error("Forms not found");
+        }
     } else {
         // Kullanıcı oturumu kapatıldıysa login sayfasına yönlendirin
         console.log("User not logged in");
