67aada4164a72da37d1d392295641d3f9953cd22
batuhanozgun <144790489+batuhanozgun@users.noreply.github.com>
2024-07-01 23:52:23 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,19 +6,23 @@ const auth = getAuth();
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
+        console.log("User logged in:", user);
         loadUsers();
         loadKayitTipleri();
         loadKayitYonleri();
         document.getElementById('kayitTipiForm').addEventListener('submit', (e) => {
             e.preventDefault();
+            console.log("Kayıt Tipi Formu Gönderildi");
             addKayitTipi();
         });
         document.getElementById('kayitYonuForm').addEventListener('submit', (e) => {
             e.preventDefault();
+            console.log("Kayıt Yönü Formu Gönderildi");
             addKayitYonu();
         });
     } else {
         // Kullanıcı oturumu kapatıldıysa login sayfasına yönlendirin
+        console.log("User not logged in");
         window.location.href = 'login.html';
     }
 });
